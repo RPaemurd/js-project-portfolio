5bdd63c7fdb523154243d57882d1c0c91f60d287
Rebecca Sighed Paemurd <rebecca.paemurd@gmail.com>
2025-11-19 15:00:46 +0100

    created skills and projects files

---
+++ src/data/projects.jsx
@@ -0,0 +1,17 @@
+export const projects = [
+    {
+        id: 1,
+        title: "Recipe Library",
+        desc: "Built in Javacsript...",
+        img: "",
+        tags: ["HTML", "CSS", "JS"]
+    },
+
+    {
+        id: 2,
+        title: "Weather API",
+        desc: "Web app built with React...",
+        img: "",
+        tags: ["HTML", "CSS", "Typescript"]
+    },
+]
---
+++ src/data/skills.jsx
@@ -0,0 +1,6 @@
+export const skillList = {
+    code: ["HTML5", "CSS3", "JavaScript ES6", "React"],
+    toolbox: ["Figma", "Slack", "VScode"],
+    upcoming: ["Node.js"],
+    more: ["Agile", "Concept Development"]
+}
