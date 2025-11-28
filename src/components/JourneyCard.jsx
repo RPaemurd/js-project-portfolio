bb8372699e80fd47b4cc03db07a4810966cc7ca9
Rebecca Sighed Paemurd <rebecca.paemurd@gmail.com>
2025-11-28 09:01:51 +0100

    made one component for cardstyles.js

--- src/components/JourneyCard.jsx
+++ src/components/JourneyCard.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { CardWrapper, ImageWrapper } from "../assets/styles/CardStyles";
 import Button from "./Button";
 
 function JourneyCard({ img, title, desc, url }) {
@@ -20,44 +21,7 @@ function JourneyCard({ img, title, desc, url }) {
 
 export default JourneyCard;
 
-const CardWrapper = styled.article`
-    display: flex;
-    flex-direction: column;
-    width: 306px;
-    min-height: 380px;
-    margin-bottom: 1rem;
 
-    h3 {
-        margin: 0.4rem 0;
-    };
-
-    p {
-        margin: 1rem 0;
-    };
-`;
-
-const ImageWrapper = styled.div`
-    position: relative;
-    height: 200px;
-    width: 100%;
-
-    &::after {
-        content: '';
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        background-color: #f581292b;
-        mix-blend-mode: multiply;
-    }
-
-    img {
-        width: 100%;
-        height: 100%;
-        display: block;    
-    }
-`;
 
 const ButtonWrapper = styled.div`
     display: flex;
--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { CardWrapper, ImageWrapper } from "../assets/styles/CardStyles";
 import Button from "./Button";
 
 function ProjectCard({ img, title, desc, netlifyUrl, githubUrl}) { //props
@@ -21,46 +22,7 @@ function ProjectCard({ img, title, desc, netlifyUrl, githubUrl}) { //props
 
 export default ProjectCard
 
-const CardWrapper = styled.article`
-    display: flex;
-    flex-direction: column;
-    width: 306px;
-    min-height: 380px;
-    margin-bottom: 1rem;
 
-    h3 {
-        margin: 0.4rem 0;
-    };
-
-    p {
-        margin: 1rem 0;
-    };
-`;
-
-const ImageWrapper = styled.div`
-
-    position: relative;
-    height: 200px;
-    width: 100%;
-
-    &::after {
-    content: '';
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-color: #f581292b;
-    mix-blend-mode: multiply;
-};
-
-    img {
-    width: 100%;
-    height: 100%;
-  /*   object-fit: cover;  */
-    display: block;    
-  };
-`;
 
 const ButtonWrapper = styled.div`
     display: flex;
